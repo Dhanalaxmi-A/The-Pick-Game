5be3f21ff365de23449f4f621205643505a4a939
Dhanalaxmi_A <dhanalaxmishettiyar261@gmail.com>
2023-08-24 21:10:45 +0530

    [Final] Implemented button functionalities and updated Read me file

--- README.md
+++ README.md
@@ -1,36 +1,23 @@
-# The-Pig-Game
+# The-Pick-Game
 
-We actually have two players.So you can play this with a friend or something or just against yourself.So the idea is that we roll a dice.
-And so now I'm playing number one.
-So this one is now the active player.
-So I roll a dice,
-And so you see that I rolled a two.
-And so this number gets added to my current score down here.
-So this is my current score for the current round.
-And this year is my total score.
-And so let's see how that's going to work so I can roll the dice again now if I want to, and now I rolled a four and that four got added to the two that it had previously.
-So now that's six and now I can choose to hold this score.
-So if I click this button, this score of the current round will get added to my total score,which right now is zero.
-And so when I hold a den, my score will be zero now.
-And we switched to player number two,
-and now I can click roll dice again.
-So we rolled a two and I can hold again.
-And so now play it two as two points.
-Now, the thing here is that whenever we roll a one,
-we lose all our current score
-and it's then the next players turn.
-And so that's why sometimes we need to hold or points.
-So let me show that to you.
-And indeed, I rolled a one right away.
-And so now it's player two again.
-So let's keep rolling the dice.
-And I will now again, wait for one so that you can see that all my current score will be lost.
-So that's a lot of points already there.
-And now I rolled a one and a player lost all the points.
-Okay. So I could have hold the points and then I would have added all these points here to my score.
-So let's do that here and again, a one.
-And so yeah, now here I hold and I edit them to these points.
-And I think by now you get how the game works, right?
-So the first player to reach 100 points then wins the game.
-But I will now not demonstrate that because that's going to take too long.So we have these two buttons here,and then we also have just like in the previous game,a way of resetting the game.
-So when I click here, as you might expect,this dice will disappear and all the scores are set back to zero.
+## Goal of the Project - A two player game where they roll the dice and hold the scores untill it reaches 100 so that the player reaching 100 would be the winner.
+
+### Reference -
+
+![Default](PickGame.png)
+
+### Steps -
+
+1. Player 1 rolls the dice. So this one is now the active player.
+2. The rolled number is a two.
+3. This number gets added to the current score down here. So the current score for the current round is two.
+4. This number gets displayed as the total score.
+5. Now, roll the dice again now if I want to, and now I rolled a four and that four got added to the two that it had previously.
+6. So now that's six and now I can choose to hold this score.
+7. Click this button, this score of the current round will get added to my total score,which right now is zero.
+8. The score will be zero now.
+9. We now switched to player number two, and I can click roll dice again.
+10. We rolled a two and I can hold again. And so now player 2 has two points.
+11. The thing here is that whenever we roll a one, we lose all our current score and it's then the next players turn. And so that's why sometimes we need to hold or points.
+12. So let's keep rolling the dice.
+13. The first player to reach 100 points then wins the game.
--- script.js
+++ script.js
@@ -13,36 +13,84 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+let scores, currentScore, activePlayer, playing;
 
-//Starting conditions
-score0Element.textContent = 0;
-score1Element.textContent = 0;
-diceElement.classList.add('hidden');
+const init = function () {
+  //Starting conditions
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0Element.textContent = 0;
+  score1Element.textContent = 0;
+  current0Element.textContent = 0;
+  current1Element.textContent = 0;
+
+  diceElement.classList.add('hidden');
+  player0Element.classList.remove('player--winner');
+  player1Element.classList.remove('player--winner');
+  player0Element.classList.add('player--active');
+  player1Element.classList.remove('player--active');
+};
+
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0Element.classList.toggle('player--active');
+  player1Element.classList.toggle('player--active');
+};
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
-  //1. Generating a random dice roll.
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    //1. Generating a random dice roll.
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  //2. Display dice roll.
-  diceElement.classList.remove('hidden');
-  diceElement.src = `dice-${dice}.png`;
+    //2. Display dice roll.
+    diceElement.classList.remove('hidden');
+    diceElement.src = `dice-${dice}.png`;
 
-  //3. Check for rolled: If true, switch to next player.
-  if (dice !== 1) {
-    //Add dice to current score
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // Switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0Element.classList.toggle('player--active');
-    player1Element.classList.toggle('player--active');
+    //3. Check for rolled: If true, switch to next player.
+    if (dice !== 1) {
+      //Add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
   }
 });
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore; // scores[1] = scores[1] + currentScore
+    console.log(scores[activePlayer]);
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 20) {
+      playing = false;
+      diceElement.classList.add('hidden');
+      // 3. Finish the game
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // 4. Switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', init);
